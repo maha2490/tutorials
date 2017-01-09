0f28f5f1ea6771020b6d2ca09e9fee4c9ff15022
Rachel Mahan <rachelm2490@gmail.com>
2017-01-09 17:45:18 -0500

    halfway upload

--- app/app.js
+++ app/app.js
@@ -1,13 +1,25 @@
+//Main file
+
 var express = require('express');
+var reload = require('reload');
 var app = express();
 var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
 
+//  static is "middlewear"
+//					|
+app.use(express.static('app/public')); //allows access to all files inside /public
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
+
+
+reload(server, app);
+
+//to run the server type node app/app.js
+//or with nodemon type nodemon app/app.js. can add in package.json file under scripts and use npm start command instead. "start": "node app/app.js" was orig code
--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -7,14 +7,18 @@ router.get('/speakers', function(req, res) {
   dataFile.speakers.forEach(function(item) {
     info += `
     <li>
+      <link rel="stylesheet" type="text/css" href="css/style.css" >
       <h2>${item.name}</h2>
+      <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker" >
       <p>${item.summary}</p>
     </li>
     `;
   });
   res.send(`
+  	  <link rel="stylesheet" type="text/css" href="css/style.css" >
       <h1>Roux Academy Meetups</h1>
       ${info}
+      <script src="/reload/reload.js"></script>
   `);
 });
 
@@ -22,9 +26,12 @@ router.get('/speakers/:speakerid', function(req, res) {
   var dataFile = req.app.get('appData');
   var speaker = dataFile.speakers[req.params.speakerid];
   res.send(`
+  	  <link rel="stylesheet" type="text/css" href="/css/style.css" >
       <h1>${speaker.title}</h1>
       <h2>with ${speaker.name}</h2>
+      <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker" >
       <p>${speaker.summary}</p>
+      <script src="/reload/reload.js"></script>
   `);
 });
 
