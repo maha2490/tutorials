1a96d73039708055d98d226b741d93e113c65c1a
Rachel Mahan <rachelm2490@gmail.com>
2017-01-09 16:29:33 -0500

    lesson 2_1 work

--- app/app.js
+++ app/app.js
@@ -4,33 +4,48 @@ var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 
+//Route for home
 app.get('/', function(req, res) {
-  var info = '';
-  dataFile.speakers.forEach(function(item) {
-    info += `
-    <li>
-      <h2>${item.name}</h2>
-      <p>${item.summary}</p>
-    </li>
-    `;
-  });
-  res.send(`
-      <h1>Roux Academy Meetups</h1>
-      ${info}
-  `);
+  
+	res.send(`
+	      <h1>Welcome</h1>
+	      <p>Roux Academy Meetups put together artists from all walks of life</p>
+	`);
+});
+
+//Route for speakers
+app.get('/speakers', function(req, res) {
+	var info = '';
+	dataFile.speakers.forEach(function (item) {
+		info += `
+			<li>
+				<h2>${item.name}</h2>
+				<p>${item.summary}</p>	
+			</li>
+		`; 
+	});
+  	res.send(`
+  			<h1>Roux Academy Meetups</h1>
+  			${info}
+  		`);
+});
+
+//Route for a specific speaker
+app.get('/speakers/:speakerid', function(req, res) {
+	//    params is an object received by the request
+	//										|
+	//										| which takes the variable declared above
+	//										|       |
+	var speaker  = dataFile.speakers[req.params.speakerid];
+  	res.send(`
+  			<h1>${speaker.title}</h1>
+  			<h2>with ${speaker.name}</h2>
+  			<p>${speaker.summary}</p>
+  		`);
 });
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
 
-// var http = require('http');
-//
-// var myServer = http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type" : "text/html"});
-//   response.write('<h1>Roux Meetups</h1>');
-//   response.end();
-// });
-//
-// myServer.listen(3000);
-// console.log('Go to http://localhost:3000 on your browser');
+
