58658a7ad45b885fe7dc4da0a6d2c293e54931e0
Rachel Mahan <rachelm2490@gmail.com>
2017-01-09 20:49:05 -0500

    added images, missing on load

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,13 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send(`
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-      <h1>Welcome to Roux Meetups</h1>
-      <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-      <p>Roux Academy Meetups put together artists from all walks of life</p>
-      <script src="/reload/reload.js"></script>
-  `);
+  var data = req.app.get('appData');
+  var pagePhotos = [];
+
+  data.speakers.forEach(function(item){
+  	pagePhotos.concat(item.artwork);
+  });
+
+  res.render('index', {
+  	pageTitle: 'Home',
+  	artwork: pagePhotos,
+  	pageID: 'home'
+  }); //don't have to specify file type, bc express looks for a file starting with index
 });
 
 module.exports = router;
